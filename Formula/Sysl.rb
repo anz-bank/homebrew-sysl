27ac1dd2f5ec3a4917775cc71671ff190ef7e01e
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-15 13:46:55 +1000

    Brew formula update for sysl version v0.109.0

--- Formula/Sysl.rb
+++ Formula/Sysl.rb
@@ -2,16 +2,16 @@
 class Sysl < Formula
   desc "Sysl generates code and documentation from system specifications"
   homepage "https://sysl.io/"
-  version "0.108.0"
+  version "0.109.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/anz-bank/sysl/releases/download/v0.108.0/sysl_0.108.0_macOS-64bit.tar.gz", :using => CurlDownloadStrategy
-    sha256 "892bf7970aaca7bd363d068d621e8b2bb86be757b1e8aa9e7b00afef90d6508a"
+    url "https://github.com/anz-bank/sysl/releases/download/v0.109.0/sysl_0.109.0_macOS-64bit.tar.gz", :using => CurlDownloadStrategy
+    sha256 "dd9411ffa314fabf90ce8228ab3882bee5063d1998e09aa948a15b466bde5e00"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/anz-bank/sysl/releases/download/v0.108.0/sysl_0.108.0_Linux-64bit.tar.gz", :using => CurlDownloadStrategy
-      sha256 "918f4da1060266e1c5161fc2b539f7cd9ca375cf560dc111fc03cf2f0bf7bdba"
+      url "https://github.com/anz-bank/sysl/releases/download/v0.109.0/sysl_0.109.0_Linux-64bit.tar.gz", :using => CurlDownloadStrategy
+      sha256 "78f7222d66de6d622a0443315ee2b8549d421e7b5fd36fe51398bd84085a372e"
     end
   end
   
